2f55a97441f1489605887372178be56ea14d0481
Celeste Magisteel <kamg1134@gmail.com>
2020-02-16 13:34:03 +0000

    Initial commit

---
+++ src/main/java/xyz/fluxinc/homemanager/HomeManager.java
@@ -0,0 +1,17 @@
+package xyz.fluxinc.homemanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomeManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
