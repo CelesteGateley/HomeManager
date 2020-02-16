1c8862e94da8416d781ef7632e4b9992b6b61b6e
Celeste Magisteel <kamg1134@gmail.com>
2020-02-16 13:44:52 +0000

    Added Command Stubs

---
+++ src/main/java/xyz/fluxinc/homemanager/commands/DeleteHomeCommand.java
@@ -0,0 +1,13 @@
+package xyz.fluxinc.homemanager.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DeleteHomeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
