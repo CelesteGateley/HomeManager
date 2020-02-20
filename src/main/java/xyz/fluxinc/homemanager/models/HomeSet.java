4c652e91b3bdeacffa83c9d972da14cc2fe06c44
Celeste Magisteel <kamg1134@gmail.com>
2020-02-20 10:02:08 +0000

    Implemented HomeSet

---
+++ src/main/java/xyz/fluxinc/homemanager/models/HomeSet.java
@@ -0,0 +1,35 @@
+package xyz.fluxinc.homemanager.models;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeSet {
+
+    private List<Home> homes;
+
+    public HomeSet() {
+        homes = new ArrayList<>();
+    }
+
+    public HomeSet addHome(Home home) {
+        homes.add(home);
+        return this;
+    }
+
+    public HomeSet addHome(Player player) {
+        homes.add(new Home(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), player.getLocation().getWorld().getName()));
+        return this;
+    }
+
+    public HomeSet addHome(Location location) {
+        homes.add(new Home(location.getX(), location.getY(), location.getZ(), location.getWorld().getName()));
+        return this;
+    }
+
+    public List<Home> getHomes() {
+        return this.homes;
+    }
+}
